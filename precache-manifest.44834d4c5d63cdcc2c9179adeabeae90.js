5656f675ddcf34c1340f1ee7c522bc316563fb00
lieucongduy182 <lieucongduy182@gmail.com>
2020-08-09 19:25:41 +0700

    Updates

---
+++ precache-manifest.44834d4c5d63cdcc2c9179adeabeae90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e700986c87b77684b2ac6c2dcb78e3fb",
+    "url": "/to-do-list-using-reactjs/index.html"
+  },
+  {
+    "revision": "591e5601c03c97a3f86f",
+    "url": "/to-do-list-using-reactjs/static/css/main.57d8bddf.chunk.css"
+  },
+  {
+    "revision": "863dc77b6ffe80bede10",
+    "url": "/to-do-list-using-reactjs/static/js/2.f8947848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-using-reactjs/static/js/2.f8947848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591e5601c03c97a3f86f",
+    "url": "/to-do-list-using-reactjs/static/js/main.6b555609.chunk.js"
+  },
+  {
+    "revision": "3953356536d484334a24",
+    "url": "/to-do-list-using-reactjs/static/js/runtime-main.f28401a9.js"
+  }
+]);
